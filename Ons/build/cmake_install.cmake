9b330f1ac21a8ac4f6a6ad6faf401f48f1604790
ZackTechnie <zakaria.ahmed6765@gmail.com>
2025-09-24 00:40:35 +0300

    hide cmd

--- Ons/build/cmake_install.cmake
+++ Ons/build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Ons")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/AzkarApp")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
